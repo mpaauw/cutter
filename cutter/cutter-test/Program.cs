c996ba5afca874694b843860e1be6ebc8e2a2be7
Matt <matt.paauw@gmail.com>
2018-02-20 20:54:36 -0800

    Project files.

---
+++ cutter/cutter-test/Program.cs
@@ -0,0 +1,19 @@
+﻿using cutter.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cutter_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService service = new WebService();
+            var item = service.GetItemAsync(16424954).Result;
+            string lol = "test";
+        }
+    }
+}
