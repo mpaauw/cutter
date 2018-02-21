6721e5e5958dd4fa1e6f00b204dd6cda1cd0f0e7
Matt <matt.paauw@gmail.com>
2018-02-20 21:09:16 -0800

    Added initial file for engine class.

---
+++ cutter/cutter/Engine/ApiEngine.cs
@@ -0,0 +1,21 @@
+﻿using cutter.Services;
+using cutter.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cutter.Engine
+{
+    public class ApiEngine
+    {
+        private WebService webService;
+
+        public ApiEngine()
+        {
+            this.webService = new WebService();
+        }
+
+    }
+}
