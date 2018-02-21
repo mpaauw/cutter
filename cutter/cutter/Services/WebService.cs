e2f76a5e91275e88602e6fe5aa8d6878be3cda15
Matt <matt.paauw@gmail.com>
2018-02-20 19:52:05 -0800

    Added initial class types, basic http service code.

---
+++ cutter/cutter/Services/WebService.cs
@@ -0,0 +1,52 @@
+﻿using cutter.Types;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cutter.Services
+{
+    public class WebService
+    {
+        private const string URL_SCHEME = "https://";
+        private const string URL_HOST = "hacker-news.firebaseio.com/";
+        private const string URL_PATH = "v0/";
+        private const string PATH_ITEM = "item/";
+
+        private HttpClient client;
+
+        public WebService()
+        {
+            this.client = new HttpClient();
+        }
+
+        public async Task<Item> GetItemAsync(int id)
+        {
+            Item item = null;
+            string endpoint = buildEndpointUrl(new string[] { URL_SCHEME, URL_HOST, URL_PATH, PATH_ITEM });
+            HttpResponseMessage response = await this.client.GetAsync(endpoint);
+            if(response.IsSuccessStatusCode)
+            {
+                item = await response.Content.ReadAsAsync<Item>();
+            }
+            else
+            {
+                throw new HttpException();
+            }
+            return item;
+        }
+
+        private string buildEndpointUrl(string[] urlParts)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach(string urlPart in urlParts)
+            {
+                builder.Append(urlPart);
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ cutter/cutter/Types/Item.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cutter.Types
+{
+    public class Item
+    {
+        /// <summary>
+        /// The item's unique id.
+        /// </summary>
+        public int id { get; set; }
+
+        /// <summary>
+        /// true if the item is deleted.
+        /// </summary>
+        public bool deleted { get; set; }
+
+        /// <summary>
+        /// The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+        /// </summary>
+        public ItemType type { get; set; }
+
+        /// <summary>
+        /// The username of the item's author.
+        /// </summary>
+        public string by { get; set; }
+
+        /// <summary>
+        /// Creation date of the item, in Unix Time.
+        /// </summary>
+        public long time { get; set; }
+
+        /// <summary>
+        /// The comment, story or poll text. HTML.
+        /// </summary>
+        public string text { get; set; }
+
+        /// <summary>
+        /// true if the item is dead
+        /// </summary>
+        public bool dead { get; set; }
+
+        /// <summary>
+        /// The comment's parent: either another comment or the relevant story.
+        /// </summary>
+        public Item parent { get; set; }
+
+        /// <summary>
+        /// The pollopt's associated poll.
+        /// </summary>
+        public int poll { get; set; }
+
+        /// <summary>
+        /// The ids of the item's comments, in ranked display order.
+        /// </summary>
+        public int[] kids { get; set; }
+
+        /// <summary>
+        /// The URL of the story.
+        /// </summary>
+        public string url { get; set; }
+
+        /// <summary>
+        /// The story's score, or the votes for a pollopt.
+        /// </summary>
+        public int score { get; set; }
+
+        /// <summary>
+        /// The title of the story, poll or job.
+        /// </summary>
+        public string title { get; set; }
+
+        /// <summary>
+        /// A list of related pollopts, in display order.
+        /// </summary>
+        public int[] parts { get; set; }
+
+        /// <summary>
+        /// In the case of stories or polls, the total comment count.
+        /// </summary>
+        public int descendants { get; set; }
+
+    }
+}
