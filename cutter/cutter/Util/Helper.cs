893a7742d2c1cc3f73b5ff95a63c4ac1c5976699
Matt <matt.paauw@gmail.com>
2018-02-21 21:40:46 -0800

    Added a Helper class to be used for generic operations.

---
+++ cutter/cutter/Util/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cutter.Util
+{
+    public class Helper
+    {
+        public static string buildEndpointUrl(string[] urlParts)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (string urlPart in urlParts)
+            {
+                builder.Append(urlPart);
+            }
+            return builder.ToString();
+        }
+    }
+}
